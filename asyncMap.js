cfcfa75e0c168c55c13883a765b00e88cce353db
David Friedman <dfried514@gmail.com>
2018-11-14 20:36:28 -0800

    completed both callback and promise versions of asyncMap function

--- asyncMap.js
+++ asyncMap.js
@@ -1,21 +1,3 @@
-const asyncMap = (tasks, callback) => {
-  let results = [];
-  let nextTaskIndex = 1;
-
-  const cb = data => {
-    results.push(data);
-    let nextTask = tasks[nextTaskIndex++];
-    if (nextTask) {
-      nextTask(cb);
-    } else {
-      callback(results)
-    }
-  };
-  tasks[0](cb);
-};
-
-const callback = arr => console.log('result', arr);
-
 const tasks = [
   function(cb){
     setTimeout(function(){
@@ -34,4 +16,40 @@ const tasks = [
   }
 ];
 
-asyncMap(tasks, callback); // ['one', 'two', 'three']
+const callback = arr => console.log('result', arr);
+
+const asyncMapCallbacks = (tasks, callback) => {
+  let results = [];
+  let nextTaskIndex = 1;
+
+  const cb = data => {
+    results.push(data);
+    let nextTask = tasks[nextTaskIndex++];
+    if (nextTask) {
+      nextTask(cb);
+    } else {
+      callback(results)
+    }
+  };
+  tasks[0](cb);
+};
+
+asyncMapCallbacks(tasks, callback); // ['one', 'two', 'three']
+
+//////////////
+
+const asyncMapPromises = (tasks, callback) => {
+
+  const createPromise = index => new Promise((resolve, reject) =>
+    tasks[index](resolve));
+
+  let promiseArray = [];
+
+  for (let i = 0; i < tasks.length; i++) {
+    promiseArray.push(createPromise(i));
+  }
+
+  Promise.all(promiseArray).then(callback);
+};
+
+asyncMapPromises(tasks, callback); // ['one', 'two', 'three']
